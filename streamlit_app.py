656120a6f6be89f17036910de1ce1e437f98d9c4
bhagyashree-patle <103431832+bhagyashree-patle@users.noreply.github.com>
2022-04-26 12:25:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Omega3 and blueberry oatmeal")
 streamlit.text("Kale, Spinach and Rocket Smoothie")
 streamlit.text("Hard Boiled Free-Range Egg")
+
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
